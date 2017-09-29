6d34d67604962a307d96a05d22ecf66581234227
Dan Nollette <nollette.dan@gmailc.om>
2017-09-29 14:42:45 -0700

    add currency formatting for brand price.

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,18 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  before_save(:upcase_name)
+
+  def currency_string
+    if self.price.zero?
+      return "0.00"
+    end
+    cents_string = (self.price.round(2) * 100).to_i.to_s
+    cents_string.insert(cents_string.length - 2, ".")
+  end
+private
+  def upcase_name
+    chars = self.name.split("")
+    chars[0].upcase!
+    self.name=chars.join("")
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
+  before_save(:upcase_name)
+
+private
+  def upcase_name
+    chars = self.name.split("")
+    chars[0].upcase!
+    self.name=chars.join("")
+  end
 end
