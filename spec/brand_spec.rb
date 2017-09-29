14f32ff934205d6d041795705500ad48495a144d
Dan Nollette <nollette.dan@gmailc.om>
2017-09-29 15:53:15 -0700

    add specs for brand and store

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,20 +1,36 @@
 require("spec_helper")
 
 describe(Brand) do
-  it("returns the price in a string form that is currency friendly") do
-    brand =Brand.create({price: 1.0267, name: "Bob"})
-    expect(brand.currency_string).to(eq("1.03"))
+  describe('#currency_string') do
+    it("returns the price in a string form that is currency friendly") do
+      brand =Brand.create({price: 1.0267, name: "Bob"})
+      expect(brand.currency_string).to(eq("1.03"))
+    end
+    it("returns the price in a string form that is currency friendly") do
+      brand =Brand.create({price: 0.0, name: "Bob"})
+      expect(brand.currency_string).to(eq("0.00"))
+    end
+    it("returns the price in a string form that is currency friendly") do
+      brand =Brand.create({price: 0.0043, name: "Bob"})
+      expect(brand.currency_string).to(eq("0.00"))
+    end
+    it("returns the price in a string form that is currency friendly") do
+      brand =Brand.create({price: 1000.0, name: "Bob"})
+      expect(brand.currency_string).to(eq("1000.00"))
+    end
   end
-  it("returns the price in a string form that is currency friendly") do
-    brand =Brand.create({price: 0.0, name: "Bob"})
-    expect(brand.currency_string).to(eq("0.00"))
+
+  describe ('#upcase_name') do
+    it("upcases first letter of brand name on save.") do
+      brand =Brand.create({price: 1.0267, name: "bob"})
+      expect(brand.name).to(eq("Bob"))
+    end
   end
-  it("returns the price in a string form that is currency friendly") do
-    brand =Brand.create({price: 0.0043, name: "Bob"})
-    expect(brand.currency_string).to(eq("0.00"))
-  end
-  it("returns the price in a string form that is currency friendly") do
-    brand =Brand.create({price: 1000.0, name: "Bob"})
-    expect(brand.currency_string).to(eq("1000.00"))
+
+  it("has many stores") do
+    test_brand = Brand.create({:name => "Terribly important brand"})
+    test_store1 = Store.create({:name => "Boot Locker", :brands => [test_brand]})
+    test_store2 = Store.create({:name => "Lady Boot Locker", :brands => [test_brand]})
+    expect(test_brand.stores()).to(eq([test_store1,test_store2]))
   end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,1 +1,18 @@
 require("spec_helper")
+
+describe(Store) do
+  it("has many brands") do
+    test_store = Store.create({:name => "Terribly important store"})
+    test_brand1 = Brand.create({:name => "Kuds", :stores => [test_store]})
+    test_brand2 = Brand.create({:name => "Udidus", :stores => [test_store]})
+    expect(test_store.brands()).to(eq([test_brand1,test_brand2]))
+  end
+
+
+  describe ('#upcase_name') do
+    it("upcases first letter of store name on save.") do
+      store = Store.create({name: "bob"})
+      expect(store.name).to(eq("Bob"))
+    end
+  end
+end
