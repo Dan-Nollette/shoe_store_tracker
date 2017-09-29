8896b0f914f77d6c2b89f1ae12b69fb60f738dc3
Dan Nollette <nollette.dan@gmailc.om>
2017-09-29 14:54:29 -0700

    add validations for brand and store.

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,5 +1,6 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  validates(:name, {:presence => true,  uniqueness: true, :length => { :maximum => 100 }})
   before_save(:upcase_name)
 
   def currency_string
@@ -9,7 +10,9 @@ class Brand < ActiveRecord::Base
     cents_string = (self.price.round(2) * 100).to_i.to_s
     cents_string.insert(cents_string.length - 2, ".")
   end
+
 private
+
   def upcase_name
     chars = self.name.split("")
     chars[0].upcase!
--- lib/store.rb
+++ lib/store.rb
@@ -1,11 +1,16 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
+  validates(:name, {:presence => true,  uniqueness: true, :length => { :maximum => 100 }})
   before_save(:upcase_name)
 
 private
+
   def upcase_name
     chars = self.name.split("")
     chars[0].upcase!
     self.name=chars.join("")
   end
+
 end
+
+ {:presence => true, :length => { :maximum => 50 }}
